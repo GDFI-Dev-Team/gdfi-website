ed01da05eba08f66af8a7fac410434a28e8c65c6
Angela Denise Almazan <ausdotsn50@gmail.com>
2026-06-09 09:16:05 +0800

    feat: hero section (#9)
    * chore: changed 'app' to 'src' to use app router for nextjs
    
    * docs: added content to AGENTS.md on initial styling conventions
    
    * chore: added hero section sample webp images
    
    * feat: added hero section slideshow effect
    
    * feat: added zoom-in effect for images in hero section
    Changed from 'div' to use next/image instead configuring which uses eager or lazy loading for Image optimization purposes
    
    * chore: cp resolve for pr#9

---
+++ src/app/page.tsx
@@ -0,0 +1,5 @@
+import { Hero } from '@/features/home/components/hero'
+
+export default function Home() {
+  return <Hero />
+}
---
+++ src/components/ui/section.tsx
@@ -0,0 +1,33 @@
+import { cn } from '../../lib/utils'
+type SectionMaxWidth = 'none' | 'full' | '2xl' | '4xl' | '5xl' | '6xl' | '7xl'
+
+const maxWidthClasses: Record<SectionMaxWidth, string> = {
+  none: 'max-w-none',
+  full: 'max-w-full',
+  '2xl': 'max-w-2xl',
+  '4xl': 'max-w-4xl',
+  '5xl': 'max-w-5xl',
+  '6xl': 'max-w-6xl',
+  '7xl': 'max-w-7xl',
+}
+
+export default function Section({
+  children,
+  className,
+  id,
+  maxWidth = '7xl',
+}: {
+  children: React.ReactNode
+  className?: string
+  id?: string
+  maxWidth?: SectionMaxWidth
+}) {
+  return (
+    /* Change bg-white later */
+    <section className={cn('py-12', className)} id={id}>
+      <div className={cn(maxWidthClasses[maxWidth], 'mx-auto sm:px-6 px-4')}>
+        {children}
+      </div>
+    </section>
+  )
+}
