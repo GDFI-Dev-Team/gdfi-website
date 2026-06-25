679b9fdc4d8b5b53c22e9fe7f7f1e1de4de34c6c
Schneidelstrom <jipetilla@up.edu.ph>
2026-06-25 13:25:29 +0800

    Support Us Description

--- src/app/support-us/page.tsx
+++ src/app/support-us/page.tsx
@@ -1,22 +1,15 @@
-import { Metadata } from 'next'
 import Banner from '@/components/ui/banner'
 import Section from '@/components/ui/section'
 import Heading from '@/components/ui/heading'
 import Text from '@/components/ui/text'
 import SupportOptions from '@/features/support-us/components/support-options'
 
-export const metadata: Metadata = {
-  title: 'Support Us',
-  description:
-    'Help the Guiuan Development Foundation Inc. protect marine ecosystems and build sustainable coastal livelihoods through your donations.',
-}
-
 export default function SupportUsPage() {
   return (
     <main className="flex-1 flex flex-col bg-foreground/[0.02]">
       <Banner
         title="Support Us"
-        description="Your contribution empowers coastal communities and helps preserve the rich marine biodiversity of Eastern Samar."
+        description="Stand with us to secure a sustainable future for our community and the environment"
         imgUrl="/feat-hero/hero-3.webp"
       />
 
